7ed2e9f6103ca1bca8488f4b8e440587ec95280a
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 16:11:26 +0200

    attributs, constructeur et move() de Car

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,18 @@
 
 public class Car extends AbstractVehicule {
-
+	private float volumEssence;
+	private int puissance;
+	private float CO2;
+	
+	//Constructeur
+	public Car(){
+		super();
+		this.volumEssence=0;
+		this.puissance=0;
+		this.CO2=0;
+	}
+	
+	public void move(){
+		
+	}
 }
