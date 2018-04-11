ca9246a06774d5e46089e44c1cfb0d8cb09d9666
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 15:18:56 +0200

    ajout des getters et setters de Position

--- src/AbstractVehicule.java
+++ src/AbstractVehicule.java
@@ -1,12 +1,14 @@
 
 public abstract class AbstractVehicule implements Vehicule {
 	private float nord, sud, est, ouest, speed;
-	/*public Position getPosition(){
-		return 
+	
+	// Constructeur
+	public AbstractVehicule(){
+		
 	}
 	
-	public void setSpeed(float speed);
-	
+	/*public Position getPosition(){
+		return 
 	*/
 	public void setDirection(float n, float s, float o, float e) {
 		this.nord=n;
--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,22 @@
 
 public class Position {
 	private float latittude, longitude;
+	
+	//Setters
+	public void setLatittude(float lat) {
+		this.latittude=lat;
+	}
+	
+	public void setLongitude(float lng) {
+		this.longitude=lng;
+	}
+	
+	//Getters	
+	public float getLatittude() {
+		return this.latittude;
+	}
+	
+	public float getLongitude() {
+		return this.longitude;
+	}
 }
