61e29821d77eda164a71f7e238058d4b72ce4883
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 15:08:47 +0200

    ajout des attributs lattitude et longitude de Position, des attributs nord, sud... de AbstractVehicule et implementation de setDirection

--- src/AbstractVehicule.java
+++ src/AbstractVehicule.java
@@ -1,4 +1,17 @@
 
 public abstract class AbstractVehicule implements Vehicule {
-
+	private float nord, sud, est, ouest, speed;
+	/*public Position getPosition(){
+		return 
+	}
+	
+	public void setSpeed(float speed);
+	
+	*/
+	public void setDirection(float n, float s, float o, float e) {
+		this.nord=n;
+		this.sud=s;
+		this.ouest=o;
+		this.est=e;
+	}
 }
--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,4 @@
 
 public class Position {
-
+	private float latittude, longitude;
 }
